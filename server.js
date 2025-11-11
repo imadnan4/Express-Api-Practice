71cca7366f1a41fbd73569efcdbc5bf4a58c97bf
imadnan4 <priadn41529@gmail.com>
2025-11-11 10:38:07 +0500

    cors added and invalid endpoint handaled

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import {router} from './Routes/apiRoutes.js'
+import cors from 'cors'
 
 const app = express()
 const PORT = 8000
 
+app.use(cors())
 app.use('/api', router)
+
+app.get((req,res)=>{
+    res.status(400).json({message:"Invalid Endpoint"})
+})
 app.listen(PORT,()=>console.log(`Server Running On PORT ${PORT}`))
